c06c338c3e781fc2e2c7b0869fe48b1fb76627c5
IsabelTizon <tizonarranz@gmail.com>
2023-02-19 16:55:43 +0000

    item page commented

--- item-page.php
+++ item-page.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-
+// Including error reporting, config and active session files.
 include("includes/active_session.php");
 include("includes/error-reporting.php");
 include("includes/config.php"); ?>
@@ -19,8 +19,10 @@ error_reporting(E_ALL);
 <!-- Head data -->
 
 <head>
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="css/details-record.css">
 
+    <!-- Including database conexion and head content files -->
     <?php include('includes/dbconx.php');
     include("modules/head-content.php"); ?>
 
@@ -29,13 +31,17 @@ error_reporting(E_ALL);
 <body>
     <header>
         <?php
+        //condition with isset() to check if the session is start
         if (isset($_SESSION["user_id"])) {
+            //If the saession is start include navbar logged file
             include("modules/navbar-logged.php");
         } else {
+            // if not, include navbar
             include("modules/navbar.php");
         }
         ?>
     </header>
+
     <!-- main content data  -->
     <main>
         <!-- Record -->
@@ -43,16 +49,16 @@ error_reporting(E_ALL);
             <?php
 
             $ID =  $_GET['ID'];
-
+            //Selecting data from the itemsMode tablewhen the order ID  is equal to the item selected 
             if ($stmt = $conn->prepare("SELECT * FROM itemsMode WHERE order_ID='$ID'")) {
                 $stmt->execute(); // execute sql statement
-                $result = $stmt->get_result();
+                $result = $stmt->get_result(); //getting the result
 
 
                 echo '<div class="det-record">';
                 while ($row = $result->fetch_assoc()) { //fetches one row of data from the results set. Continues until there are no more rows
 
-
+                    //Item Cards 
                     echo '<img class="img-record" alt="item picture" src=' . '"media/items/photos/' . $row['itemPic'] . '"' . '>';
 
                     echo '<div class="container-det">';
@@ -84,6 +90,7 @@ error_reporting(E_ALL);
 
     <!-- Footer data  -->
     <footer>
+        <!-- Including footer file -->
         <?php include("modules/footer.php"); ?>
     </footer>
 
