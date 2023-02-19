2a2f6e09dbb8b771e69a277ff8edf58919da415f
IsabelTizon <tizonarranz@gmail.com>
2023-02-19 15:15:22 +0000

    comment index

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
+//A session is started with the session_start() function.
 session_start();
 
 // print_r($_SESSION); //print out the sesion array ej: Array ( [user_id] => 1 )
 
-
-
+// Including error reporting file and config file
 include("includes/error-reporting.php");
 include("includes/config.php");
-
-
 ?>
 
 
@@ -26,9 +24,11 @@ error_reporting(E_ALL);
 <!-- Head data -->
 
 <head>
+  <!-- Custom CSS -->
   <link rel="stylesheet" href="css/index.css">
   <link rel="stylesheet" href="css/view-records.css">
 
+  <!-- Including database conexion and head content -->
   <?php include('includes/dbconx.php');
   include("modules/head-content.php"); ?>
 
@@ -41,9 +41,12 @@ error_reporting(E_ALL);
 <body>
   <header>
     <?php
+    //condition with isset() to check if the session is start
     if (isset($_SESSION["user_id"])) {
+      //If the saession is start include navbar logged file
       include("modules/navbar-logged.php");
     } else {
+      // if not, include navbar
       include("modules/navbar.php");
     }
     ?>
@@ -53,9 +56,12 @@ error_reporting(E_ALL);
   <main>
     <!-- Hero -->
     <?php
+    //condition with isset() to check if the session is start
     if (isset($_SESSION["user_id"])) {
+      //If the saession is start include hero logged file
       include("modules/hero-logged.php");
     } else {
+      // if not, include hero
       include("modules/hero.php");
     }
     ?>
@@ -63,9 +69,12 @@ error_reporting(E_ALL);
     <!-- Records -->
     <div class="records-home">
       <?php
+      //condition with isset() to check if the session is start
       if (isset($_SESSION["user_id"])) {
+        //If the saession is start include index logged file
         include("modules/index-logged.php");
       } else {
+        // if not, include index
         include("modules/index.php");
       }
       ?>
@@ -74,6 +83,7 @@ error_reporting(E_ALL);
 
   <!-- Footer data  -->
   <footer>
+    <!-- Including footer -->
     <?php include("modules/footer.php"); ?>
   </footer>
 
@@ -81,6 +91,7 @@ error_reporting(E_ALL);
 </body>
 
 <script>
+  //If the user press any of the buttons "sell now" or  the favourite icon and if not logged will lead the user to the welcome page
   function btnNotLogged() {
     location.href = "welcome.php";
   }
